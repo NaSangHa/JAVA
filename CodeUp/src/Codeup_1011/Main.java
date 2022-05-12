647ce53cbcceaa35ac729b204ebce260007d0109
DESKTOP-C5T6B6R\BigData <sju02092@naver.com>
2022-05-12 09:58:07 +0900

    2022/05/12 09:58

---
+++ CodeUp/src/Codeup_1011/Main.java
@@ -0,0 +1,18 @@
+package Codeup_1011;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str);
+
+	}
+
+}
