7dc87d58a0266462d0c75a176cc3166fac39ae79
DESKTOP-C5T6B6R\BigData <sju02092@naver.com>
2022-05-13 18:23:43 +0900

    2022/05/13 18:23

---
+++ CodeUp/src/Codeup_1035/Main.java
@@ -0,0 +1,14 @@
+package Codeup_1035;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("SourceTree Test");
+	}
+}
